52942d3efe4903a4e238fd010c8b55dfbd311175
MohdAmaan1802 <mohdamaan1802@gmail.com>
2024-02-13 17:40:35 +0530

    changes

--- README.md
+++ README.md
@@ -36,3 +36,41 @@ import {Component} from "path";
 (Normal JS utility functions)
 useState() -superpowerfull state variable in react
 useeffect()
+
+if no dependency array=> useEffect is called on every render
+if dependency array is empty=> useEffect is called on initial render(just once)
+if dependency array is btnName=> useEffect is called everytime when btnName is updated
+
+# two types of routing in web apps
+
+- Client Side Routing - when no network call happens cuz all the comps are already loaded(has code) just loads
+- Server Side Routing - when network call happens and whole page reloads
+
+# Redux Toolk🇮🇹
+
+-Install @reduxjs/toolkit and react-redux
+-Build Store
+-Conect Our store to our app
+-Slice (cartslice)
+-dispatch(action)
+-Selector
+
+# Types of Testing(devloper)
+
+-Unit Testing
+-Integration Testing
+-End to End Testing- e2e testing
+
+# Setting up testing
+
+-install react testing library-
+-install jest
+-install babel dependcies
+-configure babel
+-configure parcel conifg file to disable default babel transpilation
+-jest configuration
+-Jest-npx jest --init
+-install jsDom library -npm install --save-dev jest-environment-jsdom
+-install npm i -D @babel/preset-react to make JSX work in test cases
+-include @babel/preset-react inside my babel config
+-install npm i -D @testing-library/jest-dom
--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import UserContext from "./utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Cart from "./components/Cart";
+
+//import Grocery from "./components/Grocery";
 
 // const heading = React.createElement(
 //   "h1",
@@ -20,13 +31,66 @@ import Body from "./components/Body";
 // ]);
 //console.log(parent); //object
 
+//Chunking
+//Code Splitting
+//Dynamic bundling
+//Lazy loading
+//Dynamic Importing
+
+const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"));
+
 const AppLayout = () => {
+  const [userName, setUserName] = useState();
+
+  useEffect(() => {
+    //make an api call and send username and password
+    const data = {
+      name: "Mohd Amaan",
+    };
+    setUserName(data.name);
+  }, []);
   return (
-    <div className="app">
-      <Header />
-      <Body />
-    </div>
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div className="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Body /> },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      { path: "/contact", element: <Contact /> },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      { path: "/restaurants/:resId", element: <RestaurantMenu /> },
+      { path: "/cart", element: <Cart /> },
+    ],
+    errorElement: <Error />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
